a2e03f4d3c0af7764fda9e45346fa5ae7c2f96d2
Martinsanchez06 <sanchezmartin2435@gmail.com>
2024-02-14 17:32:26 -0500

    Adding backend and functions to this

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {pool} from './db.js'
+
+const app = express()
+
+app.get('/data', async (req, res) => {
+    const [result] = await pool.query('select * from products')
+    res.json(result)
+})
+
+app.listen(3004)
+console.log("Server running on port 3004");
--- index2.py
+++ index2.py
@@ -1,71 +1,103 @@
 import pandas as pd
+import requests
+
+# URL de la API a la que deseas hacer el GET
+url = 'http://localhost:3004/data'
+
+# Realizar la solicitud GET
+response = requests.get(url)
+
+def convertir_respuesta_api(respuesta_api):
+    # Inicializar un diccionario vacío para almacenar los datos
+    data = {}
+
+    # Iterar sobre los elementos de la respuesta de la API
+    for producto in respuesta_api:
+        # Iterar sobre las claves y valores de cada producto
+        for clave, valor in producto.items():
+            # Verificar si la clave ya existe en el diccionario data
+            if clave in data:
+                # Si la clave ya existe, agregar el valor a la lista correspondiente
+                data[clave].append(valor)
+            else:
+                # Si la clave no existe, crear una nueva lista con el valor
+                data[clave] = [valor]
+
+    return data
+
+# Creamos un conjunto de datos representado como un DataFrame de pandas
+data = convertir_respuesta_api(response.json())
 
 class ShirtRecommendationFromDataset:
     def __init__(self, dataset):
         self.dataset = dataset
 
-    def fit(self):
-        # Agregamos una nueva columna de "Relevancia" inicializada en 0 para todas las prendas
-        self.dataset['Relevancia'] = 0
-
-    def recommend(self, user_input):
+    def recommend(self, product_user_input):
         recommended_shirts = []
 
         # Filtramos el dataset basado en la entrada del usuario en las columnas "Nombre" y "Marca"
-        filtered_dataset = self.dataset[(self.dataset['Nombre'].str.contains(user_input, case=False)) | (self.dataset['Marca'].str.contains(user_input, case=False))]
+        filtered_dataset = self.dataset  # Inicializamos el dataset filtrado con el dataset completo
+        for key in self.dataset.keys():
+            if isinstance(self.dataset[key], str):  # Solo aplicamos a las llaves que contienen cadenas de texto
+                filtered_dataset = filtered_dataset[filtered_dataset[key].str.contains(product_user_input, case=False)]
 
         # Ordenamos el dataset filtrado por alguna métrica de relevancia, en este caso, por la columna 'Relevancia' de manera descendente
-        sorted_dataset = filtered_dataset.sort_values(by='Relevancia', ascending=False)
+        if not filtered_dataset.empty:  # Verificamos si el dataset filtrado no está vacío
+        #     sorted_dataset = filtered_dataset.sort_values(by='Relevancia', ascending=False)
 
-        # Tomamos las primeras 5 filas del dataset ordenado como nuestras recomendaciones
-        recommended_shirts = sorted_dataset.head(5)
+        #     # Tomamos las primeras 5 filas del dataset ordenado como nuestras recomendaciones
+        #     recommended_shirts = sorted_dataset.head(5)
+        # else:
+            recommended_shirts = filtered_dataset.head(5) 
 
         # Devolvemos las camisetas recomendadas al usuario
         return recommended_shirts
 
     def update_relevance(self, chosen_shirt):
         # Aumentamos la relevancia de la prenda escogida en 1
-        self.dataset.loc[self.dataset['Nombre'] == chosen_shirt, 'Relevancia'] += 1
+        self.dataset.loc[self.dataset[name_products_input] == chosen_shirt, 'Relevance'] += 1
 
         # Si la relevancia alcanza 100, la dejamos ahí
-        self.dataset.loc[self.dataset['Relevancia'] > 100, 'Relevancia'] = 100
+        self.dataset.loc[self.dataset['Relevance'] > 100, 'Relevance'] = 100
 
-# Creamos un conjunto de datos representado como un DataFrame de pandas
-data = {
-        'Nombre': ['Camiseta Azul', 'Camiseta Roja', 'Polo Blanco', 'Camisa Negra', 'Blusa Amarilla', 'Camiseta Verde', 'Pantalón Azul', 'Shorts Rojos', 'Chaqueta Negra', 'Bufanda Gris', 'Vestido Floral', 'Sweater Azul', 'Abrigo Negro', 'Camiseta Rayada', 'Blusa Blanca', 'Zapatos Negros', 'Zapatos Blancos', 'Botas Marrones', 'Gorra Roja', 'Gorra Negra', 'Falda Plisada', 'Jeans Azules', 'Calcetines Blancos', 'Calcetines Negros', 'Chaleco Gris', 'Pantalón Corto Verde', 'Camiseta Manga Larga', 'Jersey Rosa', 'Bufanda Roja', 'Gorro de Lana', 'Pantalón Deportivo', 'Sudadera Gris', 'Chaquetón Azul', 'Abrigo de Piel', 'Parka Verde', 'Pantalón Cargo', 'Cazadora Vaquera', 'Vestido de Noche', 'Blusa de Encaje', 'Chaleco Vaquero', 'Bañador Negro', 'Sombrero de Paja', 'Blazer Gris', 'Sudadera con Capucha', 'Pantalones de Cuero', 'Camiseta de Algodón', 'Vestido Elegante', 'Zapatillas Deportivas', 'Falda de Cuadros', 'Polo arcoiris'], 
-        'Marca': ['Marca2', 'Marca6', 'Marca6', 'Marca6', 'Marca2', 'Marca4', 'Marca3', 'Marca6', 'Marca2', 'Marca6', 'Marca6', 'Marca6', 'Marca3', 'Marca1', 'Marca6', 'Marca1', 'Marca7', 'Marca2', 'Marca6', 'Marca3', 'Marca3', 'Marca6', 'Marca6', 'Marca7', 'Marca5', 'Marca5', 'Marca7', 'Marca7', 'Marca4', 'Marca4', 'Marca6', 'Marca7', 'Marca4', 'Marca1', 'Marca7', 'Marca1', 'Marca5', 'Marca6', 'Marca6', 'Marca2', 'Marca3', 'Marca4', 'Marca2', 'Marca7', 'Marca3', 'Marca5', 'Marca1', 'Marca2', 'Marca7', 'Marca5']}
 
+# Inicializamos un array para almacenar las llaves
+keys_array = []
+
+# Iteramos sobre las llaves del diccionario y las agregamos al array
+for key in data.keys():
+    keys_array.append(key)
+    
 # Creamos un DataFrame de pandas a partir del conjunto de datos
 df = pd.DataFrame(data)
 
 # Creamos una instancia de la clase ShirtRecommendationFromDataset y le pasamos nuestro DataFrame como argumento
 shirt_rec_from_dataset = ShirtRecommendationFromDataset(df)
 
-# Llamamos al método fit para inicializar la relevancia de las prendas
-shirt_rec_from_dataset.fit()
-
 # Bucle principal
 while True:
     # Entrada del usuario
-    user_input = input("Ingrese una camiseta o marca: ")
+        product_user_input = input("Ingrese una camiseta o marca: ")
+        name_products_input = input("Nombre de la columna de los nombres de productos: ")
+        
 
     # Recomendamos camisetas similares a la entrada del usuario
-    recommended_shirts = shirt_rec_from_dataset.recommend(user_input)
+        recommended_shirts = shirt_rec_from_dataset.recommend(product_user_input)
 
-    # Mostramos las camisetas recomendadas al usuario
-    print("\nCamisetas recomendadas para '{}':".format(user_input))
-    print(recommended_shirts[['Nombre', 'Marca']])  # Mostramos solo las columnas "Nombre" y "Marca" de las recomendaciones
+        # Mostramos las camisetas recomendadas al usuario
+        print("\nCamisetas recomendadas para '{}':".format(product_user_input))
+        print(recommended_shirts[keys_array])  # Mostramos solo las columnas "Nombre" y "Marca" de las recomendaciones
 
     # El usuario elige una prenda entre las recomendadas
-    chosen_shirt = input("Seleccione una prenda de la lista (escriba el nombre exacto): ")
+        chosen_shirt = input("Seleccione una prenda de la lista (escriba el nombre exacto): ")
 
-    # Actualizamos la relevancia de la prenda elegida
-    shirt_rec_from_dataset.update_relevance(chosen_shirt)
+        # Actualizamos la relevancia de la prenda elegida
+        shirt_rec_from_dataset.update_relevance(chosen_shirt)
 
-    # Mostramos el estado actual de las relevancias
-    print("\nEstado actual de relevancias:")
-    print(shirt_rec_from_dataset.dataset[['Nombre', 'Relevancia']])  # Mostramos el nombre y la relevancia de todas las prendas
+        # Mostramos el estado actual de las relevancias
+        print("\nEstado actual de relevancias:")
+        print(shirt_rec_from_dataset.dataset[keys_array])  # Mostramos el nombre y la relevancia de todas las prendas
 
-    # Si se desea salir del bucle, se escribe "exit"
-    if chosen_shirt.lower() == 'exit':
-        break
+        # Si se desea salir del bucle, se escribe "exit"
+        if chosen_shirt.lower() == 'exit':
+            break
